6626fa8703c367487504e515bd5dd4eded6822a2
Immo Landwerth <immol@microsoft.com>
2019-03-07 08:46:54 -0600

    Initial commit

---
+++ Demo3/Program.cs
@@ -0,0 +1,13 @@
+﻿using Fabrikam.Logging;
+
+namespace Demo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new { First = "Immo", Last = "Landwerth" };
+            MyLogger.Instance.WriteLine(customer);
+        }
+    }
+}
